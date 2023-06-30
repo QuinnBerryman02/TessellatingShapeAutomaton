858a42a608ec80afbe8ca49156413f126eb5fdee
Quinn Berryman <quinn.berryman@ucdconnect.ie>
2023-06-30 06:17:37 +0100

    Create basic swing app with colored tiles

---
+++ Plane.java
@@ -0,0 +1,29 @@
+import java.awt.image.BufferedImage;
+import java.awt.*;
+
+public class Plane {
+    private BufferedImage image;
+    private int width,height;
+    
+    public Plane(int width, int height) {
+        this.width = width;
+        this.height = height;
+        
+        image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+
+        Color[] colors = {Color.red, Color.BLUE, Color.green};
+            
+        int n = 0;
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                Color c = colors[n++ % 3];
+                image.setRGB(i, j, c.getRGB());
+            }
+        }
+    }
+
+    public BufferedImage getImage() {
+        return image;
+    }
+    
+}
---
+++ Window.java
@@ -0,0 +1,64 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+
+public class Window extends JFrame implements MouseListener, MouseMotionListener, MouseWheelListener {  
+    DrawPanel panel;
+    Plane plane;
+    final int PLANE_SIZE = 20;
+    final int WINDOW_SIZE = 750;
+
+    public Window() {
+        super("Tessellation Shape Automaton");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        addMouseListener(this);
+        addMouseMotionListener(this);
+        addMouseWheelListener(this);
+
+        panel = new DrawPanel();
+        panel.setVisible(true);
+        
+        setSize(WINDOW_SIZE, WINDOW_SIZE);
+        getContentPane().add(panel);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setVisible(true);
+
+        plane = new Plane(PLANE_SIZE, PLANE_SIZE);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {}
+
+    @Override
+    public void mouseEntered(MouseEvent e) {}
+
+    @Override
+    public void mouseExited(MouseEvent e) {}
+
+    @Override
+    public void mouseMoved(MouseEvent e) {}
+
+    @Override
+    public void mouseDragged(MouseEvent e) { 
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {}
+
+    @Override
+    public void mouseWheelMoved(MouseWheelEvent e) { 
+    }
+
+    class DrawPanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            g.drawImage(plane.getImage(), 0, 0, getWidth(), getHeight(), null);
+        }
+    }
+}  
+
